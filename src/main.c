25962761be8deb43bebc09ddd68a8f278b0972ed
timothee.viossat <timothee.viossat@epita.fr>
2025-09-15 14:38:53 +0200

    exo1

--- src/main.c
+++ src/main.c
@@ -26,6 +26,10 @@ int main(int argc, char *argv[]){
 		r = _div(atof(a), atof(b));
 		printf("%lf",r);
 	}
+	else if (strcmp(op, "car") == 0) {
+		r = _car(atof(a));
+		printf("%lf",r);
+	}
 	else {printf("Erreur de parametres");}
 	}
 	else {printf("Erreur de parametres");}
